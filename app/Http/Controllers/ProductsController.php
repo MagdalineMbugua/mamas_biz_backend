439f3cfa3fdb6d24c52edb452d9383bd082ad58b
magdalinembugua <mbuguamagdaline@gmail.com>
2021-06-18 14:46:05 +0300

    implemented Product Controller

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{//fetch product_items by order of creation date
+public function index(){
+
+    $products = Product::orderby('product_name', 'desc') -> paginate(10);
+    return $products;
+
+}
+
+// add product_item
+public function store(Product $product){
+  return Product::create($product->validated());
+
+}
+
+//display a product_item
+public function show (Product $product_id){
+    return Product::findorfail($product_id);
+}
+
+//updating a product_item
+public function update (Product $product_id, Product $product){
+    return Product::findorfail($product_id) ->update($product ->validated());
+}
+
+//deleting a product_item
+public function delete(Product $product_id){
+    return Product::findorfail($product_id)->delete;
+}
+
+    //
+}
