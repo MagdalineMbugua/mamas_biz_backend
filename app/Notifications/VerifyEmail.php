1697ba13df39a68cebe92c607fc04e66116996c0
Magdaline Mbugua <empressmagda@Magdalines-MacBook-Pro.local>
2023-03-15 22:05:36 +0300

    Added mail notifications to verify and welcome new user

---
+++ app/Notifications/VerifyEmail.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Notifications;
+
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Facades\URL;
+
+class VerifyEmail extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $url = $this->createUrl($notifiable);
+        return (new MailMessage)
+            ->subject(Lang::get('Verify Email'))
+            ->line("Welcome to Mama\\'s Biz, {$notifiable->name}")
+            ->line('To start using the app, we will need to verify your email address.')
+            ->line(Lang::get('Please click the button below to verify your email address.'))
+            ->action('Verify Email Address', url($url))
+            ->line('If you did not create an account, no further action is required.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+
+    private function createUrl($notifiable): string
+    {
+        $temporarySignedUrl = URL::temporarySignedRoute(
+            'verification.verify',
+            Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
+            [
+                'id' => $notifiable->getKey(),
+                'hash' => sha1($notifiable->getEmailForVerification()),
+            ]
+        );
+
+        return $temporarySignedUrl;
+    }
+}
